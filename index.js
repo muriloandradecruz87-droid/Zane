96dc09a97c9b4c0ee12fb0560ed8a3217c3dfe1d
muriloandradecruz87-droid <muriloandradecruz87@gmail.com>
2026-04-28 10:02:39 -0300

    Update bot version to 1.20.1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function createBot() {
         host: 'Lloydsworld-LCxb.aternos.me', // Ex: meu-servidor.aternos.me
         port: 30228,                          // Mude para a sua porta de 5 números
         username: 'Zane_Bot',                 // Nome que o bot terá no jogo
-        version: 'false'                     // Mude para a versão do seu servidor
+        version: '1.20.1'                     // Mude para a versão do seu servidor
     })
 
     bot.on('spawn', () => {
